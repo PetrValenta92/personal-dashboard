e4878b9585e6e140e43e0054bb5c9eef6041dcef
Petr Valenta <petr.valenta.92@gmail.com>
2024-01-06 21:38:06 +0100

    Add and display author of the background image

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ const response = await fetch(`https://apis.scrimba.com/unsplash/photos/random?or
 
 const data = await response.json();
 
+// console.log(data);
+
 document.body.style.backgroundImage = `url('${data.urls.full}')`;
+
+document.getElementById('author').textContent = `By: ${data.user.name}`;
 
