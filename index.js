f0f4dfd35b8297f790912380b276975f8a2cd9de
Petr Valenta <petr.valenta.92@gmail.com>
2024-01-07 21:54:08 +0100

    Add try, catch for errors

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-const response = await fetch(`https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=space`)
+async function render() {
+    // fetch call is awaited
+    try {
+        const response = await fetch(`https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=space`);
 
-const data = await response.json();
+        if (!response.ok) {
+            throw new Error(`HTTP error: ${response.status}`);
+        }
 
-// console.log(data);
+        const data = await response.json();
 
-document.body.style.backgroundImage = `url('${data.urls.full}')`;
+        // console.log(data.urls.full);
+        // console.log(data.user.name);
 
-document.getElementById('author').textContent = `By: ${data.user.name}`;
+        if (data) {
+            document.body.style.backgroundImage = `url('${data.urls.regular}')`;
+            document.getElementById('author').textContent = `By: ${data.user.name}`;
+        }
+
+    } catch (err) {
+        console.error(`THIS Error message: ${err.message}`);
+        document.body.style.backgroundImage = 
+        `url('https://images.unsplash.com/photo-1528818955841-a7f1425131b5?crop=entropy&cs=srgb&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MDQ2NjA1ODB8&ixlib=rb-4.0.3&q=85')`;
+        document.getElementById('author').textContent = `By: Felix Mittermeier`;
+    }
+}
+
+render();
 
